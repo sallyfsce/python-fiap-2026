9114a0ca372fa6bd9f8218adfee27398341d8a62
sallyfsce <aneliza875@gmail.com>
2026-03-16 22:45:30 -0300

    aula 2 de python variaveis etc tal

---
+++ aula02-variaveis-operadores/Desafio.py
@@ -0,0 +1,19 @@
+#Desafio1
+
+nome = input("Digite seu nome: ")
+print(f"Boas vindas, {nome}")
+
+# Desafio 2
+
+dia = input("Digite o dia que você nasceu: ")
+mês = input("Digite o mês que você nasceu: ")
+anus = input("Digite o ano que você nasceu: ")
+
+print(f"{dia} / {mês} / {anus}")
+
+#Desafio 3
+
+numero1 = int(input("Escreva um número inteiro: "))
+numero2 = int(input("Escreva outro número inteiro: "))
+soma = numero1 + numero2
+print(soma)
---
+++ aula02-variaveis-operadores/aula02-variaveis-operadores.py
@@ -0,0 +1,32 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENA STRINGS
+
+# Comentários de 1 linha no python
+
+'''
+    Comentários de 
+    múltiplas 
+    linhas
+'''
+
+
+# VARIÁVEIS
+
+nome = "Alexandre"
+idade = 26 #int
+peso = 70.2 #float
+
+print(nome, idade, peso)
+
+# INPUT - SIMULAR UM FORMULÁRIO NO CMD
+
+nome = input("Digite seu nome: ") #string
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+print(nome, idade, peso)
+print(f"Oiii, {nome}")
+
+# Desafio 1
