826add08a2ef4526cc23c30346bdceef1547e5b7
sallyfsce <aneliza875@gmail.com>
2026-04-09 23:06:00 -0300

    att dos exercicios

--- aula02-variaveis-operadores/exercicios-aula04.py
+++ aula02-variaveis-operadores/exercicios-aula04.py
@@ -7,5 +7,38 @@ novamente.
 ▪ Se sim, exiba novamente. Senão, saia do loop e exiba a mensagem “Fim”.
 '''
 
-print('Olá, mundo.')
-print('Deseja exibir a mensagem novamente? Se sim, digite 1. Senão, digite 0.')
+while True:
+    print('Olá, mundo!')
+
+    resposta = input('Deseja exibir a mensagem novamente? (s/n): ')
+
+    if resposta.lower() != 's':
+        break
+
+print('Fim')
+
+'''
+EXERCÍCIO 2
+▪ Contagem de 0 a 100 pulando de 10 em 10.
+▪ O terminal deve ficar assim:
+0
+10
+20
+30
+40
+50
+60
+70
+80
+90
+100
+'''
+
+for i in range(0, 101, 10):
+    print(i)
+
+'''
+Faça um programa que receba um número n
+▪ Exiba a tabuada deste número do 0 ao 25.
+▪ Utilize laços de repetição
+'''
